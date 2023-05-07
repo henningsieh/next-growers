646f0a1d0c268019f0db3e64eb76a84ddf61b90d
Henning Sieh <kontakt@henningsieh.de>
2023-05-07 17:01:13 +0200

    feat: (SSG) getStaticProps at public reports\[id]

--- src/pages/reports/[id].tsx
+++ src/pages/reports/[id].tsx
@@ -2,54 +2,72 @@ import type {
   GetServerSidePropsContext,
   InferGetServerSidePropsType,
 } from "next";
+import type {
+  GetStaticPaths,
+  GetStaticPropsContext,
+  InferGetStaticPropsType,
+} from "next";
 
 import { appRouter } from "~/server/api/root";
 import { createInnerTRPCContext } from "~/server/api/trpc";
 import { createServerSideHelpers } from "@trpc/react-query/server";
+import { prisma } from "~/server/db";
 import superjson from "superjson";
 import { api as trpc } from "~/utils/api";
 
-export async function getServerSideProps(
-  context: GetServerSidePropsContext<{ id: string }>
+export async function getStaticProps(
+  context: GetStaticPropsContext<{ id: string }>
 ) {
   const helpers = createServerSideHelpers({
     router: appRouter,
     ctx: createInnerTRPCContext({ session: null }),
     transformer: superjson,
   });
-  const id = context.params?.id as string;
+  const reportId = context.params?.id as string;
 
   /*
    * Prefetching the `reports.getReportById` query here.
    */
-  await helpers.reports.getReportById.prefetch(id);
-  console.log("SERVER");
+  await helpers.reports.getReportById.prefetch(reportId);
+  console.log("getStaticProps running... 📠");
+
   // Make sure to return { props: { trpcState: helpers.dehydrate() } }
   return {
     props: {
       trpcState: helpers.dehydrate(),
-      id,
+      id: reportId,
     },
+    revalidate: 1,
   };
 }
-
+export const getStaticPaths: GetStaticPaths = async () => {
+  const reports = await prisma.report.findMany({
+    select: {
+      id: true,
+    },
+  });
+  return {
+    paths: reports.map((report) => ({
+      params: {
+        id: report.id,
+      },
+    })),
+    // https://nextjs.org/docs/pages/api-reference/functions/get-static-paths#fallback-blocking
+    fallback: "blocking",
+  };
+};
 export default function ReportDetails(
-  props: InferGetServerSidePropsType<typeof getServerSideProps>
+  props: InferGetStaticPropsType<typeof getStaticProps>
 ) {
-  const { id } = props;
-
+  const { id: reportId } = props;
   // This query will be immediately available as it's prefetched.
-  const result = trpc.reports.getReportById.useQuery(id);
-
+  const result = trpc.reports.getReportById.useQuery(reportId);
   const { data: report } = result;
-
   return (
     <>
       <h1>{report?.title}</h1>
-      <em>Created {report?.createdAt.toLocaleDateString()}</em>
-
+      <p>Created {report?.createdAt.toLocaleDateString()}</p>
       <p>{report?.description}</p>
-
       <h2>Raw data:</h2>
       <pre>{JSON.stringify(report, null, 4)}</pre>
     </>
