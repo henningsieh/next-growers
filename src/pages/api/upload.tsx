373109d8348c3f82671578d33a2b5d4386207483
Henning Sieh <kontakt@henningsieh.de>
2023-05-10 13:48:12 +0200

    fix (hopefully): no file saving to local harddrive

--- src/pages/api/upload.tsx
+++ src/pages/api/upload.tsx
@@ -41,28 +41,51 @@ const handler: NextApiHandler = async (req, res) => {
     await fs.mkdir(path.join(process.cwd() + "/public", "/images"));
   }
 
-  const data = await readFile(req, true);
+  const data = await readFile(req, false);
+
+  console.log("readFile", data);
 
   if (!!data.files.image && !Array.isArray(data.files.image)) {
     // handle the case where image is NOT an array
 
     // console.log(data.files.image.filepath);
     // console.log(data.files.image.newFilename);
-    // console.log(data.files.image.originalFilename);
+    console.log(data.files.image.originalFilename);
     // console.log(data.files.image.mimetype);
 
-    const image = data.files.image.filepath;
+    /**
+     * building publicId which will lead to a URL as follows:
+     * https://res.cloudinary.com/CLOUDNAME/image/upload/versioning_id/ORIGINALFILENAME_DATE(NOW).jpg
+     */
 
-    const result = await cloudinary.uploader.upload(image, {
-      quality: "auto", // auto transformation destroys exif data ✅
+    const now = new Date();
+    const year = now.getFullYear();
+    const month = now.getMonth() + 1;
+    const day = now.getDate();
+    const hours = now.getHours();
+    const minutes = now.getMinutes();
+    const seconds = now.getSeconds();
+
+    const formattedTimestamp = `${year}${month.toString().padStart(2, "0")}${day
+      .toString()
+      .padStart(2, "0")}${hours.toString().padStart(2, "0")}${minutes
+      .toString()
+      .padStart(2, "0")}${seconds.toString().padStart(2, "0")}`;
+
+    const publicIdWithTimestamp = `${formattedTimestamp}_${
+      data.files.image.originalFilename?.split(".")[0] as string
+    }`;
+
+    const localPathToImage = data.files.image.filepath;
+
+    const result = await cloudinary.uploader.upload(localPathToImage, {
+      public_id: publicIdWithTimestamp,
+      quality: "auto", // ⚠️ auto transformation destroys exif data ✅
       fetch_format: "auto",
-      flags: "lossy",
+      flags: "lossy", // let's save some data, quality seems fine
       invalidate: true, // invalidate cache in case, image gets updated
-      use_filename: true, // To tell Cloudinary to use the original name of the uploaded file as its public ID, include the use_filename parameter and set it to true
-      unique_filename: false, // prefix identifier
-      media_metadata: false,
     });
-    console.log(`✅ Successfully uploaded ${image}`);
+    console.log(`✅ Successfully uploaded ${localPathToImage}`);
     console.log(`URL: ${result.secure_url}`);
 
     // return successfully the new image cloud url
