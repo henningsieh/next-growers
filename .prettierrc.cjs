ab0552887bf5265c225290ca1e84d114a615a0e6
Henning Sieh <kontakt@henningsieh.de>
2023-05-26 22:33:50 +0200

    husky / prettier prepare

---
+++ .prettierrc.cjs
@@ -0,0 +1,13 @@
+const config = {
+  semi: true,
+  tabWidth: 2,
+  printWidth: 64,
+  trailingComma: "es5",
+  singleQuote: false,
+  jsxSingleQuote: false,
+  arrowParens: "avoid",
+  useTabs: false,
+  endOfLine: "auto",
+};
+
+module.exports = config;
