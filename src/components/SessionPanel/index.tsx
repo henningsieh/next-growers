2e2e4bfaec0fb6d2a02d101993898370af1c6750
Henning Sieh <kontakt@henningsieh.de>
2024-04-18 10:39:14 +0200

    feat: nicer colored menu

--- src/components/SessionPanel/index.tsx
+++ src/components/SessionPanel/index.tsx
@@ -1,6 +1,7 @@
 import {
   ActionIcon,
   Button,
+  createStyles,
   Group,
   Menu,
   rem,
@@ -11,7 +12,7 @@ import {
   IconLogout,
   IconPlant,
   IconSquarePlus,
-  IconUser,
+  IconUserEdit,
 } from "@tabler/icons-react";
 import { IconLogin2 } from "@tabler/icons-react";
 
@@ -25,11 +26,52 @@ import { useRouter } from "next/router";
 
 import { LoginModal } from "~/components/Atom/LoginModal";
 
+const useStyles = createStyles((theme) => ({
+  dropdown: {
+    backgroundColor:
+      theme.colorScheme === "dark"
+        ? theme.fn.lighten(theme.colors.dark[6], 0.0)
+        : theme.fn.lighten(theme.colors.growgreen[5], 0.7),
+  },
+  label: {
+    fontSize: theme.fontSizes.sm,
+    fontWeight: "bold",
+    color: theme.colorScheme === "dark" ? theme.white : theme.black,
+  },
+  item: {
+    color: theme.white,
+    padding: rem(8),
+    marginBottom: rem(2),
+    backgroundColor:
+      theme.colorScheme === "dark"
+        ? theme.colors.dark[4]
+        : theme.colors.gray[7],
+    "&[data-hovered]": {
+      backgroundColor: theme.colors[theme.primaryColor][4],
+      color: theme.white,
+    },
+  },
+  divider: {
+    borderTop: `2px ${
+      theme.colorScheme === "dark"
+        ? theme.colors.gray[8]
+        : theme.colors.gray[5]
+    }`,
+    borderStyle: "solid",
+  },
+}));
+
 export default function SessionPanel() {
   const router = useRouter();
+
   const { locale: activeLocale } = router;
   const { t } = useTranslation(activeLocale);
+
+  const { classes } = useStyles();
   const { data: session, status } = useSession();
+
+  const { colorScheme } = useMantineColorScheme();
+  const dark = colorScheme === "dark";
 
   const [opened, { open, close }] = useDisclosure(false);
 
@@ -47,17 +89,21 @@ export default function SessionPanel() {
     void redirectToEditAccount();
   }, [session, router, status]);
 
-  const { colorScheme } = useMantineColorScheme();
-  const dark = colorScheme === "dark";
-  // const theme = useMantineTheme();
-
   return (
     <>
       <LoginModal opened={opened} close={close} />
 
       <Group position="center">
         {status === "authenticated" ? (
-          <Menu shadow="xl" width={200}>
+          <Menu
+            position="bottom-end"
+            offset={+2}
+            withArrow
+            arrowPosition="side"
+            classNames={classes}
+            shadow="xl"
+            width={225}
+          >
             <Menu.Target>
               <ActionIcon
                 className="cursor-default overflow-hidden"
@@ -84,16 +130,16 @@ export default function SessionPanel() {
               </ActionIcon>
             </Menu.Target>
 
-            <Menu.Dropdown>
+            <Menu.Dropdown className="mr-4" mr={45}>
               <Menu.Label>Grows</Menu.Label>
               <Link href="/account/grows">
-                <Menu.Item icon={<IconPlant size={14} />}>
+                <Menu.Item icon={<IconPlant size={20} />}>
                   {t("common:usermenu-mygrows")}
                 </Menu.Item>
               </Link>
 
               {/* <Menu.Item
-                icon={<IconPhoto size={14} />}
+                icon={<IconPhoto size={20} />}
                 // Shotcut Indicator
                 rightSection={
                   <Text size="xs" color="dimmed">
@@ -106,8 +152,8 @@ export default function SessionPanel() {
 
               <Link href="/account/grows/create">
                 <Menu.Item
-                  color="green"
-                  icon={<IconSquarePlus size={14} />}
+                  bg="growgreen.5"
+                  icon={<IconSquarePlus size={20} />}
                 >
                   {t("common:usermenu-addnewgrow")}
                 </Menu.Item>
@@ -116,7 +162,7 @@ export default function SessionPanel() {
               <Menu.Divider />
               <Menu.Label>Profile</Menu.Label>
               <Link href="/account/edit">
-                <Menu.Item icon={<IconUser size={14} />}>
+                <Menu.Item icon={<IconUserEdit size={20} />}>
                   {t("common:usermenu-myprofile")}
                 </Menu.Item>
               </Link>
@@ -129,8 +175,15 @@ export default function SessionPanel() {
                     callbackUrl: `/${activeLocale as string}/grows`,
                   })
                 }
-                color="groworange.5"
-                icon={<IconLogout size={14} />}
+                // bg="red.9"
+                sx={(theme) => ({
+                  // subscribe to color scheme changes
+                  "&:hover": {
+                    backgroundColor: theme.colors.red[9],
+                    color: theme.white,
+                  },
+                })}
+                icon={<IconLogout size={20} />}
               >
                 {t("common:usermenu-logout")}
               </Menu.Item>
