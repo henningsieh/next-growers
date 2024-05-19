b6107ce437ee8c1ffa060115567ec25f0eefab15
Henning Sieh <admin@henningsieh.de>
2024-05-19 06:17:57 +0200

    fix: plausible-analytics online again

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -42,13 +42,12 @@ export default class _Document extends Document {
             href="https://fonts.googleapis.com/css2?family=Lato:ital,wght@0,100;0,300;0,400;0,700;0,900;1,100;1,300;1,400;1,700;1,900&family=Montserrat:ital,wght@0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&family=Open+Sans:ital,wght@0,300;0,400;0,500;0,600;0,700;0,800;1,300;1,400;1,500;1,600;1,700;1,800&family=Roboto+Slab:wght@100;200;300;400;500;600;700;800;900&family=Grandstander:ital,wght@0,100..900;1,100..900&display=swap"
             rel="stylesheet"
           />
-          {/* TEMPORALLY WHILE CONTABO IS DOWN */}
-          {/* <script
+          <script
             rel="canonical"
             defer
             data-domain="growagram.com"
             src="https://cdn.growagram.com/js/script.js"
-          ></script> */}
+          ></script>
           <meta property="og:type" content="website" />
         </Head>
         <body>
