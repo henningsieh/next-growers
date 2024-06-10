966c7e5d3890328e5c85b27dc11e6c6672f8b6f0
Henning Sieh <admin@henningsieh.de>
2024-06-10 12:25:01 +0200

    fix: prettier stuff

--- sentry.server.config.ts
+++ sentry.server.config.ts
@@ -1,7 +1,6 @@
 // This file configures the initialization of Sentry on the server.
 // The config you add here will be used whenever the server handles a request.
 // https://docs.sentry.io/platforms/javascript/guides/nextjs/
-
 import * as Sentry from "@sentry/nextjs";
 
 Sentry.init({
@@ -15,5 +14,4 @@ Sentry.init({
 
   // Uncomment the line below to enable Spotlight (https://spotlightjs.com)
   // spotlight: process.env.NODE_ENV === 'development',
-  
 });
